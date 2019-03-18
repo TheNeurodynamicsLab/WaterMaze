a90b4640837c266ea0e5cb502a0a76967ee360ca
Neurodynamics Lab <theneurodynamicslab@gmail.com>
2019-03-18 18:09:30 +0530

    Changed the int to Math.round in createImage and CreateImgData methods. Math.Round (new Float (float[])) for uniformity and createImgData yidx was getting substractedf by one. Now it has been removed.

--- src/watermaze_ver_3_0/WaterMazeMainFrame.java
+++ src/watermaze_ver_3_0/WaterMazeMainFrame.java
@@ -491,7 +491,8 @@ private File currDataFile; // // stores the current data file that is being used
             xidx = (dataX[count] > 0 && dataX[count] < 1 ) ? 1 : Math.round(new Float(dataX[count]));
             yidx = (dataY[count] > 0 && dataY[count] < 1) ? 1 : Math.round(new Float(dataY[count]));
             if(xidx < xMax && yidx < yMax && xidx > 0 && yidx > 0)
-                Image[((yidx-1)*xMax)+ xidx] += 10;
+                //Image[((yidx-1)*xMax)+ xidx] += 10;
+             Image[((yidx)*xMax)+ xidx] += 10;
         }
         return Image.clone();
 
